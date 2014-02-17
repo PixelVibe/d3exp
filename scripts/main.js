cc48d6bb366edd2d7d6db2a5bc560046e14d8935
Nikos Kapatos <nikos@dolphinsonline.gr>
2014-02-17 14:12:20 +0200

    require.js with cdn fallback for d3 lib

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+requirejs.config({
+    enforceDefine: false,
+    paths: {
+        d3: [
+            '//d3js.org/d3.v3.min',
+            '../lib/d3.min'
+        ]
+    }
+});
+
+require(['d3'], function (d3) {
+
+  var data = [4, 8, 15, 16, 23, 42];
+
+  d3.select('#chart').selectAll('div').data(data).enter().append('div').style('width', function(d) {return d * 10 + 'px'}).text(function(d) {return d;})
+
+
+});
---
+++ scripts/require.cfg.js
@@ -0,0 +1,9 @@
+requirejs.config({
+    enforceDefine: true,
+    paths: {
+        d3: [
+            '//ajax.aspnetcdn.com/ajax/jquery/jquery-2.0.0.min',
+            'js/jquery-2.0.0.min'
+        ]
+    }
+});
