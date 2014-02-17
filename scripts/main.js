88ce5895ca1dd084649dfb479315b29b8ca794e8
Nikos Kapatos <kapatos@gmail.com>
2014-02-17 15:02:59 +0200

    minor changes

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ requirejs.config({
     enforceDefine: false,
     paths: {
         d3: [
-            '//d3js.org/d3.v3.min',
+            //'//d3js.org/d3.v3.min',
             '../lib/d3.min'
         ]
     }
@@ -12,7 +12,6 @@ require(['d3'], function (d3) {
 
   var data = [4, 8, 15, 16, 23, 42];
 
-  d3.select('#chart').selectAll('div').data(data).enter().append('div').style('width', function(d) {return d * 10 + 'px'}).text(function(d) {return d;})
-
+  d3.select('.bar-chart').selectAll('div').data(data).enter().append('div').style('width', function(d) {return d * 10 + 'px'}).text(function(d) {return d;})
 
 });
